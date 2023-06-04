b9920bc58b020c78113031681d1a88164d774a3c
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-06-04 08:26:25 -0300

    Projeto iniciado, semana 1 feita

---
+++ aluraflix/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Video(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    url = models.URLField()
+
+    def __str__(self):
+        return self.titulo
