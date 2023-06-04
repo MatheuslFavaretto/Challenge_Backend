5e0928901e312f10c7fe2ac73541a43731e1d86b
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-06-04 11:43:04 -0300

    Adicionado Categoria

--- aluraflix/serializers.py
+++ aluraflix/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from .models import Video
+from .models import Categoria, Video
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ['id', 'titulo', 'cor']
 
 class VideoSerializer(serializers.ModelSerializer):
+    categoriaId = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all(), source='categoria', required=False)
+
     class Meta:
         model = Video
-        fields = ['id', 'titulo', 'descricao', 'url']
+        fields = ['id', 'categoriaId', 'titulo', 'descricao', 'url']
+
+    def create(self, validated_data):
+        categoria_id = validated_data.pop('categoriaId', None)
+        if not categoria_id:
+            categoria_livre, _ = Categoria.objects.get_or_create(id=1, titulo='LIVRE', cor='')
+            validated_data['categoria'] = categoria_livre
+        else:
+            validated_data['categoria'] = categoria_id
+        return super().create(validated_data)
